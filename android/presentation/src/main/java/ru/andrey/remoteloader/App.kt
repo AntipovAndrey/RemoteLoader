393c79723aa5c2d67b412cc6b287c29f6d756b62
AntipovAndrey <antipov11.11@gmail.com>
2019-02-02 21:28:34 +0300

    [android] provide base url via build config

--- android/presentation/src/main/java/ru/andrey/remoteloader/App.kt
+++ android/presentation/src/main/java/ru/andrey/remoteloader/App.kt
@@ -5,6 +5,7 @@ import ru.andrey.remoteloader.di.ApplicationComponent
 import ru.andrey.remoteloader.di.DaggerApplicationComponent
 import ru.andrey.remoteloader.di.remote.DaggerRemoteLoaderComponent
 import ru.andrey.remoteloader.di.remote.RemoteLoaderComponent
+import ru.andrey.remoteloader.di.remote.RemoteLoaderDataModule
 
 class App : Application() {
 
@@ -17,6 +18,7 @@ class App : Application() {
     val remoteLoaderComponent: RemoteLoaderComponent by lazy {
         DaggerRemoteLoaderComponent.builder()
             .applicationComponent(appComponent)
+            .remoteLoaderDataModule(RemoteLoaderDataModule(BuildConfig.API_URL))
             .build()
     }
 }

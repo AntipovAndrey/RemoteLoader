bc61d17b03839e848e05b2635794d7bf1a62e61d
AntipovAndrey <antipov11.11@gmail.com>
2019-02-03 22:30:43 +0300

    [client] action creators for fetching and request files list

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,18 @@
-import {FETCH_DEVICES} from './types';
+import {FETCH_DEVICES, FETCH_FILE_PATHS, REQUEST_FILE_PATHS} from './types';
 import remote from '../api/remote';
 
 export const fetchDevices = () => async dispatch => {
   const res = await remote.get('/devices/all');
   dispatch({type: FETCH_DEVICES, payload: res.data});
+};
+
+export const fetchFilePaths = deviceId => async dispatch => {
+  const res = await remote.get(`/paths/recent/${deviceId}`);
+  dispatch({type: FETCH_FILE_PATHS, payload: res.data});
+};
+
+export const requestFilePaths = deviceId => async dispatch => {
+  const queryListAction = {action: "QUERY_LIST"};
+  await remote.post(`/commands/save/${deviceId}`, [queryListAction]);
+  dispatch({type: REQUEST_FILE_PATHS, payload: {...queryListAction, deviceId}});
 };

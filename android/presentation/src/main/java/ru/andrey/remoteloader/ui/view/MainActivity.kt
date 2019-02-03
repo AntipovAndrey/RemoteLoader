3c26d4147622b312f0aaf8b6ad37c76d68ef0f79
AntipovAndrey <antipov11.11@gmail.com>
2019-02-03 15:28:36 +0300

    [android] add missing permission

--- android/presentation/src/main/java/ru/andrey/remoteloader/ui/view/MainActivity.kt
+++ android/presentation/src/main/java/ru/andrey/remoteloader/ui/view/MainActivity.kt
@@ -1,8 +1,30 @@
 package ru.andrey.remoteloader.ui.view
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import ru.andrey.remoteloader.ui.SingleFragmentActivity
+
 
 class MainActivity : SingleFragmentActivity() {
 
     override fun createFragment() = MainFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        askReadPermission()
+    }
+
+    private fun askReadPermission() {
+        if (Build.VERSION.SDK_INT < 23) {
+            return
+        }
+        val permission = Manifest.permission.READ_EXTERNAL_STORAGE
+        if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        ActivityCompat.requestPermissions(this, arrayOf(permission), 3)
+    }
 }

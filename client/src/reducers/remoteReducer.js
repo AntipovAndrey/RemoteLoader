be397fa3f2142c62350c060746a6dc1f9ee21b50
AntipovAndrey <antipov11.11@gmail.com>
2019-02-03 22:31:08 +0300

    [client] reducer for fetching and request files list

--- client/src/reducers/remoteReducer.js
+++ client/src/reducers/remoteReducer.js
@@ -1,13 +1,37 @@
-import {FETCH_DEVICES} from '../actions/types';
+import {FETCH_DEVICES, REQUEST_FILE_PATHS, FETCH_FILE_PATHS} from '../actions/types';
 
 const INITIAL_STATE = {
-  devices: []
+  devices: [],
+  files: {},
+  sentCommands: []
 };
+
+function filesReducer(state = INITIAL_STATE.files, action) {
+  switch (action.type) {
+    case FETCH_FILE_PATHS:
+      return {...state, [action.payload.deviceId]: {data: action.payload}};
+    default:
+      return state
+  }
+}
+
+function sentCommandsReducer(state = INITIAL_STATE.sentCommands, action) {
+  switch (action.type) {
+    case REQUEST_FILE_PATHS:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FETCH_DEVICES:
-      return {state, devices: action.payload};
+      return {...state, devices: action.payload};
+    case FETCH_FILE_PATHS:
+      return {...state, files: filesReducer(state, action)};
+    case REQUEST_FILE_PATHS:
+      return {...state, sentCommands: sentCommandsReducer(state, action)};
     default:
       return state
   }

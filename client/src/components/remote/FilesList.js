936d1c8e638bd13eab7a50f762bfc11da34b8787
AntipovAndrey <antipov11.11@gmail.com>
2019-02-04 00:00:39 +0300

    [client] implement FilesList to show device's files

--- client/src/components/remote/FilesList.js
+++ client/src/components/remote/FilesList.js
@@ -1,21 +1,69 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+import {fetchFilePaths, requestFilePaths} from '../../actions';
+
 
 class FilesList extends Component {
 
-  render() {
+  componentDidMount() {
+    this.props.fetchFilePaths(this.props.deviceId)
+  }
+
+  renderLoading() {
+    return <h2>loading</h2>;
+  }
+
+  renderNoContent() {
     return (
-      <div>{this.props.deviceId}</div>
+      <div>
+        <h2>No files for this device found</h2>
+        <button onClick={this.onFilesRequested} className="ui button primary">Request files list</button>
+      </div>
     );
+  }
+
+  renderFilesList(files) {
+    return files.map(file => {
+        return <div>{file.path}</div>
+      }
+    );
+  }
+
+  renderListRequested() {
+    return <div>Files list requested</div>;
+  }
+
+  onFilesRequested = () => {
+    this.props.requestFilePaths(this.props.deviceId)
+  };
+
+  render() {
+    if (!this.props.files) {
+      return this.renderLoading()
+    }
+    const emptyList = this.props.files.filesInfo.length === 0;
+    if (emptyList && this.props.listRequested) {
+      return this.renderListRequested()
+    }
+    if (emptyList) {
+      return this.renderNoContent()
+    }
+    return this.renderFilesList(this.props.files.filesInfo)
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(ownProps);
+  const deviceId = ownProps.match.params.deviceId;
   return {
-    deviceId: ownProps.match.params.deviceId
+    deviceId,
+    files: state.remote.files[deviceId],
+    listRequested: state.remote
+      .sentCommands
+      .filter(item => item.deviceId === deviceId)
+      .filter(item => item.action === 'QUERY_LIST')
+      .length !== 0
   }
 };
 
-export default connect(mapStateToProps, {})(FilesList);
+export default connect(mapStateToProps, {fetchFilePaths, requestFilePaths})(FilesList);
